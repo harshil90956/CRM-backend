00a0413a818406d71b1ec05d85573368d80d33ad
Akshit Gajera <akshitgajera96@gmail.com>
2026-01-07 10:41:14 +0530

    feat(payments): basic CRUD endpoints (phase 7.1)

--- api/src/modules/payments/payments.module.ts
+++ api/src/modules/payments/payments.module.ts
@@ -1,1 +1,15 @@
 // TODO: Implemented in Phase 7+ after domain blueprint approval
+
+import { Module } from '@nestjs/common';
+
+import { CoreModule } from '../../core/core.module';
+
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+
+@Module({
+  imports: [CoreModule],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
