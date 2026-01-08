a853416592329c9d681dca7d7075412f140d3557
DEVANSHEE CHODVADIYA <devanshee.chodvadiya@gmail.com>
2026-01-08 12:31:38 +0530

    devc lead completed

--- api/src/modules/leads/dto/create-lead.dto.ts
+++ api/src/modules/leads/dto/create-lead.dto.ts
@@ -1,11 +1,13 @@
+import { LeadPriority, LeadSource, LeadStatus } from '@prisma/client';
+
 export class CreateLeadDto {
   name: string;
   email: string;
   phone: string;
-  status?: string;
-  source?: string;
-  priority?: string;
-  budget?: string;
+  status: LeadStatus;
+  source: LeadSource;
+  priority?: LeadPriority;
+  budget: string;
   notes?: string;
   projectId?: string;
   assignedToId?: string;
